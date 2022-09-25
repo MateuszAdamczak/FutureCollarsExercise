7c50ee94f74dc94847883ad23f9696ce3107e540
Mateusz <maadamczak@onet.pl>
2022-09-25 19:57:09 +0200

    dodano zadanie 1 z lekcji 4

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja4/zadanie1/WindowMessage.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lekcja4.zadanie1;
+
+public class WindowMessage extends Message{
+
+    @Override
+    public String getMessage() {
+        return "This is class WindowMessage";
+    }
+}
---
+++ futurecollars/src/main/java/com/futurecollars/lekcja4/zadanie1/main.java
@@ -0,0 +1,13 @@
+package com.futurecollars.lekcja4.zadanie1;
+
+public class main {
+    public static void main(String[] args) {
+
+        WindowMessage WindowMessage = new WindowMessage();
+        System.out.println(WindowMessage.getMessage());
+
+       ConsoleMessage ConsoleMessage = new ConsoleMessage();
+        System.out.println(ConsoleMessage.getMessage());
+
+    }
+}
