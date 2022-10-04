6838309d0389f635a7767fed65bbc7ee22682d90
Mateusz <maadamczak@onet.pl>
2022-10-04 12:16:53 +0200

    dodano zadanie 4 z lekcji 4

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja4/zadanie4/ToLowerCaseFormatter.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lekcja4.zadanie4;
+
+public class ToLowerCaseFormatter implements TextFormatter{
+
+    @Override
+    public String formatText(String text) {
+        return text.toLowerCase();
+    }
+}
