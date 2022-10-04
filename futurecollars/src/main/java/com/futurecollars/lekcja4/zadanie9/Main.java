f9a7fcfdb0bb675d7b4f733b1a8fb5222ad7342d
Mateusz <maadamczak@onet.pl>
2022-10-04 12:23:04 +0200

    dodano zadanie 9 z lekcji 4

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja4/zadanie9/Main.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lekcja4.zadanie9;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(1, 2);
+        System.out.println(triangle.toString());
+    }
+}
