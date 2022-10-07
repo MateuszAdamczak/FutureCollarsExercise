f303e7c72a8113245269c8f2a4074466483927a0
Mateusz Adamczak <111048962+MateuszAdamczak@users.noreply.github.com>
2022-10-07 12:42:23 +0200

    dodano zadanie 2 z lekcji 5 (#19)

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja5/zadanie2/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lekcja5.zadanie2;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.fizzBuzzGame();
+    }
+}
