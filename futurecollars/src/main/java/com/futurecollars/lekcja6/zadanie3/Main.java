a9d234d1ab3ffa085116b3e9b4d5f108ba2991ea
Mateusz <maadamczak@onet.pl>
2022-10-11 00:21:18 +0200

    dodano zadanie 3 z lekcji 6

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie3/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lekcja6.zadanie3;
+
+public class Main {
+    public static void main(String[] args) {
+        CharacterCounter numberOfChar = new CharacterCounter();
+        numberOfChar.printNumberChar();
+    }
+}
