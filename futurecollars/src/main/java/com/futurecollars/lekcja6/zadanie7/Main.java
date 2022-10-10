0dd2c268082072deb40921e4c63847b25c43f5cf
Mateusz <maadamczak@onet.pl>
2022-10-11 01:02:52 +0200

    dodano zadanie 6 z lekcji 7

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie7/Main.java
@@ -0,0 +1,12 @@
+package com.futurecollars.lekcja6.zadanie7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Element elemental2 = new Element(16);
+        elemental2.changeNumberIntoRoot();
+
+        Element elemental = new Element(-16);
+        elemental.changeNumberIntoRoot();
+    }
+}
