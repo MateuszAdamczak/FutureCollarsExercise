17dd93460453468fd1a0cca882c1de715d309d7d
Mateusz <maadamczak@onet.pl>
2022-10-11 00:22:54 +0200

    dodano zadanie 4 z lekcji 6

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie4/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lekcja6.zadanie4;
+
+public class Main {
+    public static void main(String[] args) {
+        WordFinder searchForWord = new WordFinder();
+        searchForWord.getSearchWord("Wyjątki");
+    }
+}
---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie4/WordFinder.java
@@ -0,0 +1,20 @@
+package com.futurecollars.lekcja6.zadanie4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WordFinder {
+    public void getSearchWord(String text) {
+        try {
+            Path path = Paths.get("src/main/java/com/futurecollars/lekcja6/zadanie4/data.txt");
+            String content = Files.readString(path);
+            boolean Word = content.contains(text);
+            System.out.println(content);
+            System.out.println(Word);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
