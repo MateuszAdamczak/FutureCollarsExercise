23ca6ac6adbd7d6dc2c8e18fc516ec47eb0de99a
Mateusz <maadamczak@onet.pl>
2022-10-11 00:20:15 +0200

    dodano zadanie 2 z lekcji 6

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie2/Main.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lekcja6.zadanie2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        FileReader fileReader = new FileReader();
+        fileReader.getTheSecondLine();
+    }
+}
