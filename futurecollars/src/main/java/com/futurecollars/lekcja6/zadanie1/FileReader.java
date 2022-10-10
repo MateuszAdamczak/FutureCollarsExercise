f1c67984ed27c8ded6e7cb1325dd771433b46486
Mateusz <maadamczak@onet.pl>
2022-10-11 00:19:28 +0200

    dodano zadanie 1 z lekcji 6

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie1/FileReader.java
@@ -0,0 +1,18 @@
+package com.futurecollars.lekcja6.zadanie1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileReader {
+    public void openAndWriteFile() {
+        try {
+            Path path = Paths.get("src/main/java/com/futurecollars/lekcja6/zadanie1/data.txt");
+            String content = Files.readString(path);
+            System.out.println(content);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
