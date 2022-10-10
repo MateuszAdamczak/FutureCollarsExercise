74b094ca005085a7aec475dd678c94dcddd498e9
Mateusz <maadamczak@onet.pl>
2022-10-11 01:03:49 +0200

    dodano zadanie 6 z lekcji 8

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie8/WrongTypeOfDataException.java
@@ -0,0 +1,7 @@
+package com.futurecollars.lekcja6.zadanie8;
+
+public class WrongTypeOfDataException extends RuntimeException {
+    public WrongTypeOfDataException(String massage) {
+        super(massage);
+    }
+}
