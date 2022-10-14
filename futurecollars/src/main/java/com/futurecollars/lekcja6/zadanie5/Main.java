792289926fa593d2287d85be464455bd1937d863
Mateusz Adamczak <111048962+MateuszAdamczak@users.noreply.github.com>
2022-10-14 11:25:06 +0200

    dodano zadanie 5 z lekcji 6 (#28)

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja6/zadanie5/Main.java
@@ -0,0 +1,8 @@
+package com.futurecollars.lekcja6.zadanie5;
+
+public class Main {
+    public static void main(String[] args) {
+        QueueChanger inversionOfTheContent = new QueueChanger();
+        inversionOfTheContent.getReverseOrder();
+    }
+}
