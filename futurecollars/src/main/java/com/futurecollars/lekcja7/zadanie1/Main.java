1b116f3a2d5fac1bad87da73e7e3228ab4ec5960
Mateusz <maadamczak@onet.pl>
2023-01-09 11:24:56 +0100

    dodano zadanie 1 z lekcji 7

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja7/zadanie1/Main.java
@@ -0,0 +1,11 @@
+package com.futurecollars.lekcja7.zadanie1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DivisibleNumber divisibleNumber = new DivisibleNumber();
+        System.out.println(divisibleNumber.isDivisibleNumber(10));
+        SumOfGivenNumber sumOfGivenNumber = new SumOfGivenNumber();
+        System.out.println(sumOfGivenNumber.getSumOfGivenNumber(123));
+    }
+}
