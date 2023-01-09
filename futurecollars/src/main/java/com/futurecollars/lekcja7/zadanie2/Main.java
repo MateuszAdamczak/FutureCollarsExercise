8d7472731a6680bbbcd89a57cbf403116999609f
Mateusz <maadamczak@onet.pl>
2023-01-09 11:26:20 +0100

    dodano zadanie 2 z lekcji 7

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja7/zadanie2/Main.java
@@ -0,0 +1,10 @@
+package com.futurecollars.lekcja7.zadanie2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        UpperCaseTextFormatter upperCaseTextFormatter = new UpperCaseTextFormatter();
+        System.out.println(upperCaseTextFormatter.convertToUpperCase("nowy rok"));
+    }
+}
+
---
+++ futurecollars/src/main/java/com/futurecollars/lekcja7/zadanie2/UpperCaseTextFormatter.java
@@ -0,0 +1,11 @@
+package com.futurecollars.lekcja7.zadanie2;
+
+public class UpperCaseTextFormatter {
+    public String convertToUpperCase(String text) {
+        if (text == null) {
+            return "";
+        }
+        return text.toUpperCase();
+    }
+}
+
