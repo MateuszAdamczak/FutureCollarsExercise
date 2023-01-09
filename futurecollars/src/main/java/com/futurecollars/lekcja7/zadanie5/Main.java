b5871f4adea5611126e860a168587f46c11cdb46
Mateusz <maadamczak@onet.pl>
2023-01-09 11:27:02 +0100

    dodano zadanie 5 z lekcji 7

---
+++ futurecollars/src/main/java/com/futurecollars/lekcja7/zadanie5/Main.java
@@ -0,0 +1,10 @@
+package com.futurecollars.lekcja7.zadanie5;
+
+import com.futurecollars.lekcja7.zadanie5.NameMonth;
+
+public class Main {
+    public static void main(String[] args) {
+        NameMonth nameMonth = new NameMonth();
+        System.out.println(nameMonth.getNameMonth(2));
+    }
+}
---
+++ futurecollars/src/main/java/com/futurecollars/lekcja7/zadanie5/NameMonth.java
@@ -0,0 +1,9 @@
+package com.futurecollars.lekcja7.zadanie5;
+
+import java.time.Month;
+
+public class NameMonth {
+    public Month getNameMonth(int numberMonth) {
+        return Month.of(numberMonth);
+    }
+}
